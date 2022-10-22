79318949e0f0a98261257655ea66df17dd797b76
itWelt <rassulovmurad@gmail.com>
2022-10-22 15:08:14 +0400

    [master][task] added Position

--- app/Imports/SipuniUsersImport.php
+++ app/Imports/SipuniUsersImport.php
@@ -22,12 +22,20 @@ class SipuniUsersImport implements ToCollection, WithHeadingRow
             // echo '</pre><br>';
 
             $explodedName = explode(' ', $row['name']);
+            $name         = '';
+
+            for ($i = 2; $i < count($explodedName); $i++) {
+                $name .= $explodedName[$i] . ' ';
+            }
 
             // echo '<pre>'; print_r($explodedName); echo '</pre><br>';
 
             User::create(array_merge($row->toArray(), [
-                'number' => $row['login'],
-                'department' => User::getDepartment($explodedName[0])
+                'name'       => $name,
+                'number'     => $row['login'],
+                'department' => mb_strtolower($explodedName[0]) !== 'пусто' ? mb_strtolower($explodedName[0]) : null,
+                'position'   => mb_strtolower($explodedName[1]) !== 'пусто' ? mb_strtolower($explodedName[1]) : null,
+                // 'department' => User::getDepartment($explodedName[0])
             ]));
         }
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -26,6 +26,7 @@ class User extends Authenticatable
         'name',
         'number',
         'department',
+        'position',
         'ats',
         'tag',
         'label',
